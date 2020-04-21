22604364ca9b377d261cce705180f4fa6466f103
Anna Vardanyan <62400603+avard18@users.noreply.github.com>
2020-04-21 19:08:05 +0400

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,49 @@
+let don = document.getElementById("don");
+let re = document.getElementById("re");
+let mi = document.getElementById("mi");
+let fa = document.getElementById("fa");
+let sol = document.getElementById("sol");
+let la = document.getElementById("la");
+let si = document.getElementById("si");
+let dod=document.getElementById("dod");
+let red=document.getElementById("red");
+let fad=document.getElementById("fad");
+let sold=document.getElementById("sold");
+let lad=document.getElementById("lad");
+
+function playDon(){
+    don.play();
+}
+function playRe(){
+    re.play();
+}
+function playMi(){
+    mi.play();
+}
+function playFa(){
+    fa.play();
+}
+function playSol(){
+    sol.play();
+}
+function playLa(){
+    la.play();
+}
+function playSi(){
+    si.play();
+}
+function playDod(){
+    dod.play();
+}
+function playRed(){
+    red.play();
+}
+function playFad(){
+    fad.play();
+}
+function playSold(){
+    sold.play();
+}
+function playLad(){
+    lad.play();
+}
